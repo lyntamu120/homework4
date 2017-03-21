b47f64d4f723dc941976b966e2a979a2e61eeb66
Xining Wang <tamuwin2016@tamu.edu>
2017-03-21 17:36:05 -0500

    modify in production.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rottenpotatoes::Application.routes.draw do
-
+  root "movies#index"
   get "movies/show_same_director"
-
 
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  #root :to => redirect('/movies')
 
 
 end
